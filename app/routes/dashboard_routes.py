7c5746edc32cbac84a67ddf74a6756d67ef336fc
aaronfx <74271892+aaronfx@users.noreply.github.com>
2026-03-06 17:13:50 +0100

    Update dashboard_routes.py

--- app/routes/dashboard_routes.py
+++ app/routes/dashboard_routes.py
@@ -3,6 +3,7 @@ from flask_login import login_required
 
 dashboard = Blueprint("dashboard", __name__)
 
+
 @dashboard.route("/")
 def home():
     return render_template("index.html")
@@ -30,18 +31,3 @@ def analysis():
 @login_required
 def mentor():
     return render_template("mentor.html")
-
-
-@dashboard.route("/blog")
-def blog():
-    return render_template("blog.html")
-
-
-@dashboard.route("/webinars")
-def webinars():
-    return render_template("webinars.html")
-
-
-@dashboard.route("/courses")
-def courses():
-    return render_template("courses.html")
