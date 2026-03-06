0ece03c3f4b1af34bebd7c7d6e620f490be8f95e
aaronfx <74271892+aaronfx@users.noreply.github.com>
2026-03-06 12:54:31 +0100

    Create _init-.py

---
+++ app/_init-.py
@@ -0,0 +1,30 @@
+
+from flask import Flask
+from .extensions import db, login_manager, bcrypt
+from .config import Config
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    db.init_app(app)
+    login_manager.init_app(app)
+    bcrypt.init_app(app)
+
+    from .routes.auth_routes import auth
+    from .routes.dashboard_routes import dashboard
+    from .routes.blog_routes import blog
+    from .routes.course_routes import courses
+    from .routes.webinar_routes import webinars
+    from .routes.analysis_routes import analysis
+    from .routes.admin_routes import admin
+
+    app.register_blueprint(auth)
+    app.register_blueprint(dashboard)
+    app.register_blueprint(blog)
+    app.register_blueprint(courses)
+    app.register_blueprint(webinars)
+    app.register_blueprint(analysis)
+    app.register_blueprint(admin)
+
+    return app
