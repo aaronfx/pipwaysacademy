2a179825f2acac8b75487f9d805dbc3af1efe3d4
aaronfx <74271892+aaronfx@users.noreply.github.com>
2026-03-06 16:09:08 +0100

    Update user_model.py

--- app/models/user_model.py
+++ app/models/user_model.py
@@ -1,10 +1,18 @@
 from ..extensions import db
 from flask_login import UserMixin
+from datetime import datetime
+
 
 class User(db.Model, UserMixin):
 
+    __tablename__ = "users"
+
     id = db.Column(db.Integer, primary_key=True)
 
-    email = db.Column(db.String(150), unique=True)
+    email = db.Column(db.String(150), unique=True, nullable=False)
 
-    password = db.Column(db.String(200))
+    password = db.Column(db.String(255), nullable=False)
+
+    is_admin = db.Column(db.Boolean, default=False)
+
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
