92a5e08199ba70ce753e169c8e6a2e5d9b6ba506
aaronfx <74271892+aaronfx@users.noreply.github.com>
2026-03-06 13:12:14 +0100

    Update webinar_routes.py

--- app/routes/webinar_routes.py
+++ app/routes/webinar_routes.py
@@ -1,1 +1,11 @@
+from flask import Blueprint, render_template
+from app.models.webinar_model import Webinar
 
+webinars = Blueprint("webinars", __name__)
+
+@webinars.route("/webinars")
+def webinar_list():
+
+    all_webinars = Webinar.query.all()
+
+    return render_template("webinars.html", webinars=all_webinars)
