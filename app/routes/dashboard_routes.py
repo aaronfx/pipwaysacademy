7bb98ddd2609279e34f7abbdb99504327d8bce76
aaronfx <74271892+aaronfx@users.noreply.github.com>
2026-03-06 16:10:45 +0100

    Update dashboard_routes.py

--- app/routes/dashboard_routes.py
+++ app/routes/dashboard_routes.py
@@ -3,12 +3,9 @@ from flask_login import login_required
 
 dashboard = Blueprint("dashboard", __name__)
 
+
 @dashboard.route("/")
-def home():
-    return render_template("index.html")
-
-
-@dashboard.route("/dashboard")
 @login_required
-def dashboard_page():
+def home():
+
     return render_template("dashboard.html")
