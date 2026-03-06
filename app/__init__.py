fd2676d6dba89719cad1c92be13801d78151fca9
aaronfx <74271892+aaronfx@users.noreply.github.com>
2026-03-06 13:58:47 +0100

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,35 +1,34 @@
 from flask import Flask
 from .extensions import db, login_manager
 
+
 def create_app():
 
     app = Flask(__name__)
 
-    app.config["SECRET_KEY"] = "pipways-secret-key"
+    app.config["SECRET_KEY"] = "supersecret"
     app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
     db.init_app(app)
     login_manager.init_app(app)
 
-    # Import models
+    # IMPORT MODELS BEFORE create_all
     from .models.user_model import User
-    from .models.blog_model import Post
     from .models.course_model import Course, CourseEnrollment
+    from .models.blog_model import Post
 
-    # Import routes
+    # IMPORT ROUTES
     from .routes.auth_routes import auth
     from .routes.dashboard_routes import dashboard
     from .routes.blog_routes import blog
-    from .routes.course_routes import course
+    from .routes.course_routes import courses
 
-    # Register routes
     app.register_blueprint(auth)
     app.register_blueprint(dashboard)
     app.register_blueprint(blog)
-    app.register_blueprint(course)
+    app.register_blueprint(courses)
 
-    # Create database tables
     with app.app_context():
         db.create_all()
 
