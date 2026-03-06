957a42c0ce9b8f8b5c104a40607583de886637c6
aaronfx <74271892+aaronfx@users.noreply.github.com>
2026-03-06 13:46:53 +0100

    Update auth_routes.py

--- app/routes/auth_routes.py
+++ app/routes/auth_routes.py
@@ -1,30 +1,28 @@
-from flask import Blueprint, render_template, request, redirect, url_for, flash
-from flask_login import login_user, logout_user, login_required
-from app.extensions import db, bcrypt
-from app.models.user_model import User
+from flask import Blueprint, render_template, request, redirect, url_for
+from flask_login import login_user, logout_user
+from ..models.user_model import User
+from ..extensions import db
 
 auth = Blueprint("auth", __name__)
 
-@auth.route("/login", methods=["GET", "POST"])
+@auth.route("/login", methods=["GET","POST"])
 def login():
 
     if request.method == "POST":
+
         email = request.form.get("email")
         password = request.form.get("password")
 
         user = User.query.filter_by(email=email).first()
 
-        if user and bcrypt.check_password_hash(user.password, password):
+        if user and user.password == password:
             login_user(user)
-            return redirect(url_for("dashboard.dashboard"))
-
-        flash("Invalid login credentials")
+            return redirect(url_for("dashboard.dashboard_page"))
 
     return render_template("login.html")
 
 
 @auth.route("/logout")
-@login_required
 def logout():
     logout_user()
     return redirect(url_for("auth.login"))
