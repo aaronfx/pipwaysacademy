30c8b832b1d647d6b7d23b145e677e50894d5106
aaronfx <74271892+aaronfx@users.noreply.github.com>
2026-03-06 13:46:16 +0100

    Update dashboard_routes.py

--- app/routes/dashboard_routes.py
+++ app/routes/dashboard_routes.py
@@ -1,6 +1,5 @@
 from flask import Blueprint, render_template
-from flask_login import login_required, current_user
-from app.models.course_model import Course, CourseEnrollment
+from flask_login import login_required
 
 dashboard = Blueprint("dashboard", __name__)
 
@@ -11,14 +10,5 @@ def home():
 
 @dashboard.route("/dashboard")
 @login_required
-def dashboard_home():
-
-    courses = Course.query.all()
-
-    enrollments = CourseEnrollment.query.filter_by(user_id=current_user.id).all()
-
-    return render_template(
-        "dashboard.html",
-        courses=courses,
-        enrollments=enrollments
-    )
+def dashboard_page():
+    return render_template("dashboard.html")
